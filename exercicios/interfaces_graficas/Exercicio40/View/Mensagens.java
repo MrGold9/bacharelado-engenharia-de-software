05b747abf9a8f2a7b339bb425cbc364d22eea176
MrGold9 <dudu_jr_007@hotmail.com>
2023-11-27 00:58:03 -0300

    exercício usando interfaces gráficas e um pseudo banco de dados

---
+++ exercicios/interfaces_graficas/Exercicio40/View/Mensagens.java
@@ -0,0 +1,12 @@
+package exercicios.interfaces_graficas.Exercicio40.View;
+
+
+public class Mensagens extends Exception 
+{
+    
+    Mensagens(String msg)
+    {
+        super(msg);
+    }
+
+}
---
+++ exercicios/interfaces_graficas/Exercicio40/View/Principal.java
@@ -0,0 +1,15 @@
+package exercicios.interfaces_graficas.Exercicio40.View;
+
+
+public class Principal {
+
+
+    public static void main(String[] args) 
+    {
+        
+        TelaPrincipal objetotela = new TelaPrincipal(); //Cria objeto do JFrame TelaPrincipal
+        objetotela.setVisible(true); //Abre a TelaPrincipal
+
+    }
+    
+}
